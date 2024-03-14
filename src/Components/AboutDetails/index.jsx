71b099108a92a7e789448f1f8d1d0c1f1ea577c7
Luis Feliz <Luisfeliz@gmail.com>
2024-03-14 17:34:40 -0400

    Mobile responsive fix v1.0.13-about-page

--- src/Components/AboutDetails/index.jsx
+++ src/Components/AboutDetails/index.jsx
@@ -1,34 +1,27 @@
 import "./style.css";
 
-import me_icon from '../../image/me_suit.jpg'
+import me_icon from "../../image/me_suit.jpg";
 
 const AboutDetails = () => {
   return (
-   <div className="about-container">
-       <div className="about-detail">
-      <h1>
-       I'm Luis, holding a Bachelor's in Computer and Technology. 
-       My expertise lies in frontend engineering, where I'm dedicated to 
-       empowering communities and expressing creativity through music 
-       and software development.
-      </h1>
-      <p>
-        In most of these Projects I am Incorparating the use of M.E.R.N with the
-        MVC (Model, View, Controller) Framework.
-      </p>
-
-     
-    </div>
-    <div>
-        <div>
-          <img
-            alt="prepair_icon"
-            src={me_icon}
-            className="me-card"
-          />
-        </div>
+    <div className="about-container">
+      <div className="about-detail">
+        <h1 className="about-title">
+          I'm Luis, holding a Bachelor's in Computer and Technology. My
+          expertise lies in frontend engineering, where I'm dedicated to
+          empowering communities and expressing creativity through music and
+          software development.
+        </h1>
+        <p className="about-description">
+          In most of these Projects I am Incorparating the use of M.E.R.N with
+          the MVC (Model, View, Controller) Framework.
+        </p>
       </div>
-   </div>
+
+      <div className="about-image">
+        <img alt="me_icon" src={me_icon} className="me-card" />
+      </div>
+    </div>
   );
 };
 
