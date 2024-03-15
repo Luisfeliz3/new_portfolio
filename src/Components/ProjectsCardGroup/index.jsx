4856fe91eaf7236f562d0bbac89c8db9e8d8f33c
Luis Feliz <Luisfeliz@gmail.com>
2024-03-14 20:05:04 -0400

    Mobile responsive fix v1.0.23-project-page

--- src/Components/ProjectsCardGroup/index.jsx
+++ src/Components/ProjectsCardGroup/index.jsx
@@ -28,7 +28,7 @@ const ProjectsCardGroup = () => {
           measurements, & build custom pieces like tables, 
           chairs, & more, ensuring you avoid mistakes.
           No more hassles hunting for materials & puzzling intructions to build your unit.
-          Choose what to build, choose materials, insert measurements, click & buy !
+          Choose what to build, choose materials, measurements, click & buy !
           </p>
           <a className="app-links" href="https://github.com/Luisfeliz3/prepair">
             <svg
